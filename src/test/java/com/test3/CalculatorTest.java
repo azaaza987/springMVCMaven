b80776a1200fcd2e9a34a537f7ca169357687b11
azaaza987 <bayi_xiao@163.com>
2017-05-25 09:50:32 +0800

    Update CalculatorTest.java

--- src/test/java/com/test3/CalculatorTest.java
+++ src/test/java/com/test3/CalculatorTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 
+
+
 //@RunWith(JUnit4ClassRunner.class)
 public class CalculatorTest {
 	private static Calculator calculator = new Calculator();
